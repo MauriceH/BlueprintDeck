b8f40bf1cd65f1bf7065edb3fdeff4e0622a7683
Maurice Hessing <m.hessing@validdata.de>
2019-09-29 19:08:11 +0200

    feat: add design XY location

--- source/dotnet/BlueprintDeck.Core/Design/NodeDesignInstance.cs
+++ source/dotnet/BlueprintDeck.Core/Design/NodeDesignInstance.cs
@@ -1,7 +1,11 @@
+using System.Drawing;
+
 namespace BlueprintDeck.Design
 {
     public class NodeDesignInstance
     {
+        public Point Location { get; set; }
+        
         public string? NodeTypeKey { get; set; }
         
         public string? NodeInstanceId { get; set; }
